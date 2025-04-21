183eff8d8423755ab8b7bff5354e4304c7e2b9ac
Andikarya17 <andikaryaputra17@gmail.com>
2025-04-21 16:15:48 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("ini video Anthem Real Madrid");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("ini Audio Anthem Real Madrid");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("ini gambar Pemain Real Madrid");
+});
